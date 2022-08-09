39cb10a4ff9e7323bb77f523a8d191bfa628409f
郝先瑞 <1490493387@qq.com>
2022-08-09 08:01:47 +0800

    feat: 新增OAuth2Authentication对象序列化器

--- youlai-common/common-security/src/main/java/com/youlai/common/security/serializer/OAuth2AuthenticationMixin.java
+++ youlai-common/common-security/src/main/java/com/youlai/common/security/serializer/OAuth2AuthenticationMixin.java
@@ -3,6 +3,7 @@ package com.youlai.common.security.serializer;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
 @JsonAutoDetect(
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
         isGetterVisibility = JsonAutoDetect.Visibility.NONE
 )
 @JsonDeserialize(using = OAuth2AuthenticationDeserializer.class)
+@JsonSerialize(using = OAuth2AuthenticationSerializer.class)
 public class OAuth2AuthenticationMixin {
 
 }
